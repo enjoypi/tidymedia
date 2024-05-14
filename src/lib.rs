55ff901ef91bd53c761bd4af23f2384f9e7c3ea9
Fan <>
2024-05-14 10:17:43 +0800

    lib.rs as interface_adapters

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,73 @@
 use std::io;
 
-pub use interface_adapters::Commands;
+use clap::Subcommand;
 
-mod interface_adapters;
+mod use_cases;
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Copy non-duplicate files from the source directory to the output directory. No source files will be modified.
+    Copy {
+        /// Dry run, do not copy files
+        #[arg(short, long)]
+        dry_run: bool,
+
+        /// The source directories or files
+        #[arg(required = true)]
+        sources: Vec<String>,
+
+        /// The output directory
+        #[arg(short, long)]
+        output: String,
+    },
+
+    /// Find all duplicate files in the source directory and print a shell script (using batch file syntax for Windows) to delete the duplicate files on standard output. If the output parameter is provided, then deletion operations for files located in the output directory will be commented out.
+    Find {
+        /// Use fast hash
+        #[arg(short, long, default_value = "true", action = clap::ArgAction::SetTrue)]
+        fast: bool,
+
+        /// The source directories or files
+        #[arg(required = true)]
+        sources: Vec<String>,
+
+        /// The output directory
+        #[arg(short, long)]
+        output: Option<String>,
+    },
+
+    /// Move non-duplicate files from the source directory to the output directory. Duplicate files already present in the output directory will be deleted.
+    Move {
+        /// Dry run, do not move files
+        #[arg(short, long)]
+        dry_run: bool,
+
+        /// The source directories or files
+        #[arg(required = true)]
+        sources: Vec<String>,
+
+        /// The output directory
+        #[arg(short, long)]
+        output: String,
+    },
+}
 
 pub fn tidy(command: Commands) -> io::Result<()> {
-    interface_adapters::tidy(command)
+    match command {
+        Commands::Copy {
+            dry_run,
+            sources,
+            output,
+        } => use_cases::copy(sources, output, dry_run, false),
+        Commands::Find {
+            fast,
+            sources,
+            output,
+        } => use_cases::find_duplicates(fast, sources, output),
+        Commands::Move {
+            dry_run,
+            sources,
+            output,
+        } => use_cases::copy(sources, output, dry_run, true),
+    }
 }
